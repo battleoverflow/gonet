fc8d870488b90b61c4655137347981b4de6e6f50
Hifumi <hifumisecurity@protonmail.com>
2022-07-09 01:00:43 -0500

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Syndicate
- Network communication CLI tool for gathering network information across multiple variants
+Network communication CLI tool for gathering network information across multiple variants
+
+Syndicate is an extremely simplistic and CLI tool built in Go.
+
+Please do not take this project too seriously. This was more of an attempt at me improving my Go experience.
+
+Commands:
+```
+help            |   Help menu
+ns --host       |   Search for the nameservers of an available host
+ip --host       |   Gathers IP addresses about the specified host
+cname --host    |   Gathers CNAME information about the specified host
+mx --host       |   Gathers MX records from the specified host
+all --host      |   Prints every command in a single output
+```
+
+## Example(s)
+
+## ns
+```bash
+go run cmd/syn/syn.go ns --host WebsiteUrl
+```
+
+## ip
+```bash
+go run cmd/syn/syn.go ip --host WebsiteUrl
+```
+
+## cname
+```bash
+go run cmd/syn/syn.go cname --host WebsiteUrl
+```
+
+## mx
+```bash
+go run cmd/syn/syn.go mx --host WebsiteUrl
+```
+
+## all
+```bash
+go run cmd/syn/syn.go all --host WebsiteUrl
+```
---
+++ cmd/cli/syn.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+		controller := cli.NewApp()
+
+		controller.Name = "Syndicate"
+		controller.Usage = "Network communication CLI tool for gathering network information across multiple variants"
+
+		version := "v0.1.3"
+		author := "Hifumi1337 (https://github.com/Hifumi1337)"
+
+		defaultValues := []cli.Flag {
+			cli.StringFlag {
+				// Default values for commands
+				Name: "host",
+				Value: "google.com",
+			},
+		}
+
+		// Command List
+		controller.Commands = []cli.Command {
+		{
+			Name: "version",
+			Usage: "Version information",
+			Flags: defaultValues,
+
+		Action: func(cmd *cli.Context) error {
+				fmt.Println("Syndicate |", version)
+				fmt.Println("Author:", author)
+				
+				return nil
+			},
+		},
+		{
+			Name: "ns",
+			Usage: "Gathers nameserver information about the specified host",
+			Flags: defaultValues,
+
+			// ns command activated
+			Action: func(cmd *cli.Context) error {
+				ns, err := net.LookupNS(cmd.String("host"))
+
+				if err != nil {
+					return err
+				}
+
+				// Nameservers
+				fmt.Println("Nameserver(s):")
+				fmt.Println("==============================")
+
+				// Logs results to terminal
+				for i := 0; i < len(ns); i++ {
+					fmt.Println("[*]", ns[i].Host)
+				}
+
+				fmt.Println("==============================")
+
+				return nil
+			},
+		},
+		{
+			Name: "ip",
+			Usage: "Gathers IP addresses about the specified host",
+			Flags:defaultValues,
+
+			// ip command activated
+			Action: func(cmd *cli.Context) error {
+				ip, err := net.LookupIP(cmd.String("host"))
+
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				// IP Address(es)
+				fmt.Println("IP Address(es):")
+				fmt.Println("==============================")
+
+				// Logs results to terminal
+				for i := 0; i < len(ip); i++ {
+					fmt.Println("[*]", ip[i])
+				}
+
+				fmt.Println("==============================")
+
+				return nil
+			},
+		},
+		{
+			Name: "cname",
+			Usage: "Gathers CNAME information about the specified host",
+			Flags:defaultValues,
+
+			// cname command activated
+			Action: func(cmd *cli.Context) error {
+				cname, err := net.LookupCNAME(cmd.String("host"))
+
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				// CNAME
+				fmt.Println("CNAME:")
+				fmt.Println("==============================")
+
+				// Logs results to terminal
+				fmt.Println("[*]", cname)
+
+				fmt.Println("==============================")
+				
+				return nil
+			},
+		},
+		{
+			Name: "mx",
+			Usage: "Gathers MX records from the specified host",
+			Flags:defaultValues,
+
+			// mx command activated
+			Action: func(cmd *cli.Context) error {
+				mx, err := net.LookupMX(cmd.String("host"))
+
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				// MX Record
+				fmt.Println("MX Record(s):")
+				fmt.Println("==============================")
+
+				// Log results to terminal
+				for i := 0; i < len(mx); i++ {
+					fmt.Println("Host:", mx[i].Host)
+					fmt.Println("Priority:", mx[i].Pref)
+				}
+
+				fmt.Println("==============================")
+
+				return nil
+			},
+		},
+		{
+			Name: "all",
+			Usage: "Prints every command in a single output",
+			Flags:defaultValues,
+
+			// mx command activated
+			Action: func(cmd *cli.Context) error {
+				ns, err := net.LookupNS(cmd.String("host"))
+				ip, err := net.LookupIP(cmd.String("host"))
+				cname, err := net.LookupCNAME(cmd.String("host"))
+				mx, err := net.LookupMX(cmd.String("host"))
+
+				if err != nil {
+					fmt.Println(err)
+				}
+				
+				fmt.Println("Syndicate |", version)
+				fmt.Println("Author:", author)
+
+				fmt.Println("\n")
+
+				// Nameservers
+				fmt.Println("Nameserver(s):")
+				fmt.Println("==============================")
+
+				for i := 0; i < len(ns); i++ {
+					fmt.Println("[*]", ns[i].Host)
+				}
+
+				fmt.Println("\n")
+
+				// IP Address(es)
+				fmt.Println("IP Address(es):")
+				fmt.Println("==============================")
+
+				for i := 0; i < len(ip); i++ {
+					fmt.Println("[*]", ip[i])
+				}
+
+				fmt.Println("\n")
+
+				// CNAME
+				fmt.Println("CNAME:")
+				fmt.Println("==============================")
+				
+				fmt.Println("[*]", cname)
+				
+				fmt.Println("\n")
+
+				// MX Record
+				fmt.Println("MX Record(s):")
+				fmt.Println("==============================")
+
+				for i := 0; i < len(mx); i++ {
+					fmt.Println("Host:", mx[i].Host)
+					fmt.Println("Priority:", mx[i].Pref)
+					fmt.Println("\n")
+				}
+
+				return nil
+			},
+		},
+	}
+
+	// Initializing Engine...
+	err := controller.Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
